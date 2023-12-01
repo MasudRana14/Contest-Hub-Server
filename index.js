783910a95a7a4291c923c8a6dcdd89ff04c28103
MasudRana14 <iammasud34@gmail.com>
2023-12-02 00:56:37 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+// Middleware 
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ousunhm.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    const contestCollection = client.db('contestDB').collection('contests');
+
+
+    app.post('/contests', async (req, res) => {
+      const newContest = req.body;
+      const result = await contestCollection.insertOne(newContest);
+      res.send(result);
+    })
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.get('/', (req, res)=>{
+    res.send('Contest Hub is running')
+})
+
+app.listen(port, ()=>{
+    console.log(`Contest Hub is running on port ${port}`);
+})
