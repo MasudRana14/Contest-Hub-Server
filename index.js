17b75a6ae9e06245c0a7071d05d9479d201d1583
MasudRana14 <iammasud34@gmail.com>
2023-12-03 21:07:25 +0600

    Get Contest Single Data

--- index.js
+++ index.js
@@ -44,6 +44,14 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     })
+
+    // Get Contest Single Data 
+    app.get('/contests/:id', async (req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await contestCollection.findOne(query);
+      res.send(result);
+    })
  
     // User Related Api 
     app.get('/users', async (req, res)=>{
