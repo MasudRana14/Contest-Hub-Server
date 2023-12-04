f3462f4782d145b9711b167ba9a1218a4c51c6eb
MasudRana14 <iammasud34@gmail.com>
2023-12-04 20:40:37 +0600

    get admin email verifyToken

--- index.js
+++ index.js
@@ -85,7 +85,16 @@ async function run() {
     }
 
 
-    
+    const verifyAdmin = async ( req, res, next)=>{
+      const email = req.decoded.email;
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin){
+        return res.status(403).send({message: 'forbidden access'});
+      }
+      next();
+    }
 
 
 
