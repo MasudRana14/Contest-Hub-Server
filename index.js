eb455de6a093bf74501313aaff28da270eb3b044
MasudRana14 <iammasud34@gmail.com>
2023-12-04 20:34:40 +0600

    get jwt verifyToken

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -31,13 +32,26 @@ async function run() {
     const contestCollection = client.db('contestDB').collection('contests');
     const userCollection = client.db('contestDB').collection('users');
 
+
+    // Jwt Token 
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h'
+      });
+      res.send({token})
+    })
+
+
+
+
     // Post All Contest Data 
     app.post('/contests', async (req, res) => {
       const newContest = req.body;
       const result = await contestCollection.insertOne(newContest);
       res.send(result);
     })
-    
+
     // Get All Contest Data 
     app.get('/contests', async (req, res) => {
       const cursor = contestCollection.find();
@@ -46,35 +60,79 @@ async function run() {
     })
 
     // Get Contest Single Data 
-    app.get('/contests/:id', async (req, res)=>{
+    app.get('/contests/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await contestCollection.findOne(query);
       res.send(result);
     })
- 
+
+
+    // middlewares 
+    const verifyToken = (req, res, next)=>{
+      console.log(req.headers);
+      if(!req.headers.authorization){
+        return res.status(401).send({message: 'unauthorized access'});
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+        if(err){
+          return res.status(401).send({message: 'unauthorized access'})
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+
+    
+
+
+
+
     // User Related Api 
-    app.get('/users', async (req, res)=>{
+    app.get('/users', verifyToken, async (req, res) => {
+    
       const result = await userCollection.find().toArray();
       res.send(result);
     })
-    app.post('/users', async (req, res)=>{
+
+    app.get('/user/admin/:email', verifyToken, async(req, res)=>{
+      const email = req.params.email;
+      if(email !== req.decoded.email){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if(user){
+        admin = user?.role === 'admin';
+      }
+      res.send({admin});
+
+    })
+
+
+
+
+    app.post('/users', async (req, res) => {
       const user = req.body;
-      const query = {email: user.email}
+      const query = { email: user.email }
       const existingUser = await userCollection.findOne(query);
-      if(existingUser){
-        return res.send({message: 'User Already Exist', insertedId: null})
+      if (existingUser) {
+        return res.send({ message: 'User Already Exist', insertedId: null })
       }
       const result = await userCollection.insertOne(user);
       res.send(result);
     })
 
-
-    app.patch('/users/admin/:id', async (req, res)=>{
+    // Admin Role 
+    app.patch('/users/admin/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)};
-      const updatedDoc ={
-        $set:{
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
           role: 'admin'
         }
       }
@@ -82,11 +140,12 @@ async function run() {
       res.send(result);
     })
 
-    app.put('/users/admin/:id', async (req, res)=>{
+    // Creator Role 
+    app.put('/users/admin/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)};
-      const updatedDoc ={
-        $set:{
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
           role: 'creator'
         }
       }
@@ -95,9 +154,9 @@ async function run() {
     })
 
     // User Delete 
-    app.delete('/users/:id', async (req, res)=>{
+    app.delete('/users/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await userCollection.deleteOne(query);
       res.send(result);
     })
@@ -117,10 +176,10 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res)=>{
-    res.send('Contest Hub is running')
+app.get('/', (req, res) => {
+  res.send('Contest Hub is running')
 })
 
-app.listen(port, ()=>{
-    console.log(`Contest Hub is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Contest Hub is running on port ${port}`);
 })
